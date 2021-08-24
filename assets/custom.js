5f758fdf04957a429e18fa3f96414561453f3bc5
Antonio Nicasio <webmediadigital@gmail.com>
2021-08-23 19:52:55 -0700

    added cart fix mobile search and shop menu

--- assets/custom.js
+++ assets/custom.js
@@ -8,3 +8,27 @@ function hideAnnouncementBar() {
   var bar = document.getElementById("hide-announcement-bar").style = "display: none";
   return bar
 }
+
+function showClearSearch() {
+  var show = document.getElementById("clear-search")
+  textLength = show.value.length
+  var hidden = document.getElementById("close").style = "display: none"
+  if(show && textLength > 0 || show.value !== "") {
+    var close = document.getElementById("close").style = "display: block";
+    return close
+  }else if(show.value === "") {
+    var close = document.getElementById("close").style = "display: none";
+    return close
+  }else {
+    return hidden
+  }
+}
+
+
+function openMenuShop() {
+  document.getElementById("shopMenu").style = "display: block"
+}
+
+function closeMenuShop() {
+  document.getElementById("shopMenu").style = "display: none"
+}
