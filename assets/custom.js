d4eb0e748399b8d36693eee7e3d6f61a6e052d56
Antonio Nicasio <webmediadigital@gmail.com>
2021-09-06 20:42:11 -0700

    mega menu added

--- assets/custom.js
+++ assets/custom.js
@@ -47,6 +47,16 @@ function closeDiscoveryMenu() {
   removeBlur.classList.remove('active');
 }
 
+function openMegaMenu() {
+  document.getElementById("SiteNavLabel-face").style = "display: block";
+}
+
+function closeMegaMenu() {
+  document.getElementsByClassName("site-nav__dropdown").style = "display: none";
+  console.log('click and close')
+}
+
+
 function openChatBoot() {
   document.getElementById("gobot").style = "display: block";
   const chat = document.getElementById("gobot-wrapper")
@@ -63,66 +73,4 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-("use strict");
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-
-var NewsletterPopupSection = function () {
-  function NewsletterPopupSection(element) {
-    _classCallCheck(this, NewsletterPopupSection);
-
-    this.element = element;
-    this.delegateElement = new domDelegate.Delegate(this.element);
-    this.options = JSON.parse(element.getAttribute('data-section-settings'));
-
-    // If the popup has been already displayed, we do not display it
-    try {
-      if (window.location.hash === '#newsletter-popup' && window.theme.template !== null) {
-        this._openPopup();
-      } else if (!this.options['showOnlyOnce'] || this.options['showOnlyOnce'] && localStorage.getItem('themePopup') === null) {
-        setTimeout(this._openPopup.bind(this), this.options['apparitionDelay'] * 1000);
-      }
-    } catch (error) {
-      // Some browsers (especially in private mode) throw an exception when trying to access local storage, so we protect ourselves here
-    }
-
-    this._attachListeners();
-  }
-
-  _createClass(NewsletterPopupSection, [{
-    key: 'onUnload',
-    value: function onUnload() {
-      this.delegateElement.off();
-    }
-  }, {
-    key: 'onSelect',
-    value: function onSelect() {
-      this._openPopup();
-    }
-  }, {
-    key: 'onDeselect',
-    value: function onDeselect() {
-      this._closePopup();
-    }
-  }, {
-    key: '_attachListeners',
-    value: function _attachListeners() {
-      this.delegateElement.on('click', '[data-action="close-popup"]', this._closePopup.bind(this));
-    }
-  }, {
-    key: '_openPopup',
-    value: function _openPopup() {
-      this.element.setAttribute('aria-hidden', 'false');
-      localStorage.setItem('themePopup', 'true');
-    }
-  }, {
-    key: '_closePopup',
-    value: function _closePopup() {
-      this.element.setAttribute('aria-hidden', 'true');
-    }
-  }]);
-
-  return NewsletterPopupSection;
-}();
